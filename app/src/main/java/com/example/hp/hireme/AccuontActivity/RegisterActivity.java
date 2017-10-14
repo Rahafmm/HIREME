9a0be1dbc3f707a9a30a571fd67ac678660c3bcd
HP <rahofa.962hotmail.com>
2017-10-14 16:15:36 +0300

    test

---
+++ app/src/main/java/com/example/hp/hireme/AccuontActivity/RegisterActivity.java
@@ -0,0 +1,107 @@
+package com.example.hp.hireme.AccuontActivity;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.hp.hireme.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button buttonRgister;
+    private EditText editTextName;
+    private  EditText textEmailAddress;
+    private  EditText editTextPasword;
+    private  EditText editTextCPasword;
+    private TextView  textViewLogIn;
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        firebaseAuth= FirebaseAuth.getInstance();
+                progressDialog= new ProgressDialog(this);
+
+        buttonRgister = (Button) findViewById(R.id.buttonRgister);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        textEmailAddress = (EditText) findViewById(R.id.editTextEmail);
+        editTextPasword = (EditText) findViewById(R.id.editTextPasword);
+        editTextCPasword = (EditText) findViewById(R.id.editTextCPasword);
+        textViewLogIn = (TextView) findViewById(R.id.textviewLogIn);
+
+        buttonRgister.setOnClickListener(this);
+        textViewLogIn.setOnClickListener(this);
+    }
+
+    private  void registerUser(){
+        String Email = textEmailAddress.getText().toString().trim();
+        String password = editTextPasword.getText().toString().trim();
+        String Cpassword = editTextCPasword.getText().toString().trim();
+        String Name = editTextName.getText().toString().trim();
+
+
+        if(TextUtils.isEmpty(Email)){
+            Toast.makeText(this,"plase enter email", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"plase enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        if(TextUtils.isEmpty(Cpassword)){
+            Toast.makeText(this,"plase enter confirom password ", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        if(TextUtils.isEmpty(Name)){
+            Toast.makeText(this,"plase enter name", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        progressDialog.setMessage("Regisreing USER ....");
+        progressDialog.show();
+        firebaseAuth.createUserWithEmailAndPassword(Email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(RegisterActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "could not register , please register again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void onClick(View view){
+
+        if(view == buttonRgister ){
+            registerUser();
+        }
+        if(view == textViewLogIn ) {
+
+
+
+        }
+
+    }
+}
--- app/src/main/java/com/example/hp/hireme/HIREME.java
+++ app/src/main/java/com/example/hp/hireme/HIREME.java
@@ -2,12 +2,25 @@ package com.example.hp.hireme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HIREME extends AppCompatActivity {
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hireme);
+
+
+
     }
 }
