fe2a8645ad18d4295e2a8c92c9b1564a786f5794
HP <rahofa.962hotmail.com>
2017-10-15 17:21:57 +0300

    test

--- app/src/main/java/com/example/hp/hireme/HIREME.java
+++ app/src/main/java/com/example/hp/hireme/HIREME.java
@@ -18,7 +18,7 @@ public class HIREME extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hireme);
+        setContentView(R.layout.activity_register);
 
 
 
