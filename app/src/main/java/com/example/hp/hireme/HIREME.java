fae186b302a4d71a783f52feb04a2fb56cca484e
HP <rahofa.962hotmail.com>
2017-10-17 21:37:49 +0300

    register

--- app/src/main/java/com/example/hp/hireme/HIREME.java
+++ app/src/main/java/com/example/hp/hireme/HIREME.java
@@ -9,12 +9,15 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HIREME extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,10 +25,14 @@ public class HIREME extends AppCompatActivity implements View.OnClickListener {
 
     //defining view objects
     private EditText editTextEmail;
+    private EditText editTextName;
     private EditText editTextPassword;
+    private  EditText editTextCPasword;
     private Button buttonSignup;
-
+    private TextView  textViewLogIn;
+private DatabaseReference mDatabase;
     private ProgressDialog progressDialog;
+    private TextView regorg;
 
     //defining firebaseauth object
 
@@ -38,16 +45,20 @@ public class HIREME extends AppCompatActivity implements View.OnClickListener {
 
         //initializing firebase auth object
         firebaseAuth = FirebaseAuth.getInstance();
-
+        mDatabase= FirebaseDatabase.getInstance().getReference().child("candet");
         //initializing views
+        editTextName = (EditText) findViewById(R.id.editTextName);
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPasword);
-
+        editTextCPasword = (EditText) findViewById(R.id.editTextCPasword);
+        textViewLogIn = (TextView) findViewById(R.id.textviewLogIn);
+        regorg = (TextView) findViewById(R.id.regorg);
         buttonSignup = (Button) findViewById(R.id.buttonRgister);
 
         //attaching listener to button
         buttonSignup.setOnClickListener((View.OnClickListener) this);
-
+        textViewLogIn.setOnClickListener(this);
+        regorg.setOnClickListener(this);
         // to chow message
         progressDialog = new ProgressDialog(this);
 
@@ -55,13 +66,24 @@ public class HIREME extends AppCompatActivity implements View.OnClickListener {
 
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
+        if(view == buttonSignup ){
         //calling register method on click
-        registerUser();
+        registerUser();}
+        if(view == textViewLogIn ) {
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+
+        }
+        if(view == textViewLogIn ) {
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        }
     }
 
     private void registerUser() {
         //getting email and password from edit texts
+        final String Cpassword = editTextCPasword.getText().toString().trim();
+        final String Name = editTextName.getText().toString().trim();
+
         String email = editTextEmail.getText().toString().trim();
         String password  = editTextPassword.getText().toString().trim();
 
@@ -88,6 +110,10 @@ public class HIREME extends AppCompatActivity implements View.OnClickListener {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         //checking if success
                         if(task.isSuccessful()){
+                            String User_ID=firebaseAuth.getCurrentUser().getUid();
+                            DatabaseReference Cureent_User_db= mDatabase.child(User_ID);
+                            Cureent_User_db.child("name").setValue(Name);
+                            Cureent_User_db.child("Cpassword").setValue(Cpassword);
                             //display message to the user here
                             Toast.makeText(HIREME.this,"Successfully registered",Toast.LENGTH_LONG).show();
                             //close this activity
