c927fadff6d11cca0c2b5d57942e02f1e86622b0
HP <rahofa.962hotmail.com>
2017-10-17 14:03:15 +0300

    update

--- app/src/main/java/com/example/hp/hireme/AccuontActivity/RegisterActivity.java
+++ app/src/main/java/com/example/hp/hireme/AccuontActivity/RegisterActivity.java
@@ -1,23 +1,22 @@
 package com.example.hp.hireme.AccuontActivity;
 
 import android.app.ProgressDialog;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.hp.hireme.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
     private Button buttonRgister;
@@ -30,11 +29,13 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private ProgressDialog progressDialog;
 
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("candet");
         firebaseAuth= FirebaseAuth.getInstance();
                 progressDialog= new ProgressDialog(this);
 
@@ -81,17 +82,18 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
         progressDialog.setMessage("Regisreing USER ....");
         progressDialog.show();
-        firebaseAuth.createUserWithEmailAndPassword(Email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        firebaseAuth.createUserWithEmailAndPassword(Email,password).addOnCompleteListener(this, new OnCompleteListener<com.google.firebase.auth.AuthResult>() {
             @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
+            public void onComplete(@NonNull Task<com.google.firebase.auth.AuthResult> task) {
                 if (task.isSuccessful()) {
                     Toast.makeText(RegisterActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(RegisterActivity.this, "could not register , please register again", Toast.LENGTH_SHORT).show();
                 }
-            }
-        });
-    }
+                }
+            });
+        }
+
     public void onClick(View view){
 
         if(view == buttonRgister ){
--- app/src/main/java/com/example/hp/hireme/HIREME.java
+++ app/src/main/java/com/example/hp/hireme/HIREME.java
@@ -1,11 +1,7 @@
 package com.example.hp.hireme;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 public class HIREME extends AppCompatActivity {
 
@@ -18,7 +14,7 @@ public class HIREME extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
 
 
 
