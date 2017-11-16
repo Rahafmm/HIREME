3b29f0a87ac773d16a88d84542565d57c4cae46a
HP <rahofa.962hotmail.com>
2017-11-16 08:45:25 +0300

    upload

---
+++ app/src/main/java/com/example/hp/hireme/AccuontActivity/Constants.java
@@ -0,0 +1,10 @@
+package com.example.hp.hireme.AccuontActivity;
+
+/**
+ * Created by HP on 11/15/2017.
+ */
+
+public class Constants {
+    public static final String STORAGE_PATH_UPLOADS = "uploads/";
+    public static final String DATABASE_PATH_UPLOADS = "uploads";
+}
---
+++ app/src/main/java/com/example/hp/hireme/AccuontActivity/Upload.java
@@ -0,0 +1,29 @@
+package com.example.hp.hireme.AccuontActivity;
+
+/**
+ * Created by HP on 11/15/2017.
+ */
+
+public class Upload {
+
+    public String name;
+    public String url;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public Upload() {
+    }
+
+    public Upload(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
