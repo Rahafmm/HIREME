a0fcb0e01226b9b1a691c6211c150eced907b8a6
l8eena <32533640+l8eena@users.noreply.github.com>
2017-11-20 15:30:06 +0300

    favorite

--- app/src/main/java/com/example/hp/hireme/AccuontActivity/FavoriteActivity.java
+++ app/src/main/java/com/example/hp/hireme/AccuontActivity/FavoriteActivity.java
@@ -1,5 +1,6 @@
 package com.example.hp.hireme.AccuontActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,10 +8,16 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.hp.hireme.R;
+import com.example.hp.hireme.viewInfoOrg;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,46 +26,76 @@ import java.util.List;
 
 public class FavoriteActivity extends AppCompatActivity {
 
-    private Button buttonViewFav;
-    private Button buttonAddfav;
-    private DatabaseReference mDatabase;
-    private FirebaseAuth firebaseAuth;
-    ListView listView;
-    //list to store favorites
-    List<Upload> favList;
-    //////////////view fav
+    String cat;
+    ListView listFav;
+    DatabaseReference mDatabase;
+    ArrayList<Org> names;
+    TextView lisFav1;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favorite);
 
-        buttonViewFav = (Button) findViewById(R.id.buttonViewFav);
-        buttonViewFav.setOnClickListener((View.OnClickListener) this);
+        Intent intent=getIntent();
+        cat=intent.getStringExtra("cat");
 
-
-
-
-
-        favList = new ArrayList<>();
-        listView = (ListView) findViewById(R.id.listView);
-//////////////////add fav
-        final Button buttonAddfav=(Button)findViewById(R.id.buttonAddfav);
-        buttonAddfav.setOnClickListener(new View.OnClickListener() {
+        names=new ArrayList<>();
+        listFav=(ListView) findViewById(R.id.FavListView);
+        lisFav1=(TextView)findViewById(R.id.FavListView1);
+        //listorg1.setText(cat);
+        listFav.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                if(view==buttonAddfav){
-                    addFav();
-                }
-            }
-            public void addFav(){
-//displaying it to list
-               // ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, favList);
-               // listView.setAdapter(adapter);
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Org selOrg = names.get(i);
+
+                //Intent intent =new Intent(listOrg.this, viewInfoOrg.class);
 
             }
         });
-    }
 
+
+
+        mDatabase= FirebaseDatabase.getInstance().getReference().child("Org");
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    Org orga = postSnapshot.getValue(Org.class);
+
+                    names.add(orga);
+                }
+
+                String[] uploads = new String[names.size()];
+
+                for (int i = 0; i < uploads.length; i++) {
+
+                    uploads[i] = names.get(i).getname();
+
+                }
+
+                //displaying it to list
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, uploads);
+                listFav.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+
+    }
     }
 
 
