a35962328d60c538d9d769396789289c89e8a157
HP <rahofa.962hotmail.com>
2017-11-21 20:08:26 +0300

    edit

--- app/src/main/java/com/example/hp/hireme/ViewUploadsActivity.java
+++ app/src/main/java/com/example/hp/hireme/ViewUploadsActivity.java
@@ -1,14 +1,16 @@
 package com.example.hp.hireme;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import com.example.hp.hireme.AccuontActivity.Constants;
@@ -22,11 +24,16 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ViewUploadsActivity extends AppCompatActivity {
+public class ViewUploadsActivity extends AppCompatActivity implements View.OnClickListener {
 
     //the listview
     ListView listView;
-
+    ArrayAdapter<String> adapter;
+    String[] uploads;
+    String[] uploads1;
+    final Context context = this;
+    boolean press;
+    ImageButton undo;
     //database reference to get uploads data
     DatabaseReference mDatabaseReference;
 
@@ -41,7 +48,8 @@ public class ViewUploadsActivity extends AppCompatActivity {
 
         uploadList = new ArrayList<>();
         listView = (ListView) findViewById(R.id.listView);
-
+        undo=(ImageButton)findViewById(R.id.undo);
+        undo.setOnClickListener(this);
 
         //adding a clicklistener on listview
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -72,14 +80,14 @@ public class ViewUploadsActivity extends AppCompatActivity {
                     uploadList.add(upload);
                 }
 
-                String[] uploads = new String[uploadList.size()];
+                 uploads = new String[uploadList.size()];
 
                 for (int i = 0; i < uploads.length; i++) {
                     uploads[i] = uploadList.get(i).getName();
                 }
 
                 //displaying it to list
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, uploads);
+                 adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, uploads);
                 listView.setAdapter(adapter);
             }
 
@@ -88,7 +96,88 @@ public class ViewUploadsActivity extends AppCompatActivity {
 
             }
         });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view,
+                                           int position, long id) {
+                removeItemFromList(position);
+                return true;
+
+
+            }
+
+        });
+
     }
+    protected void removeItemFromList(int position) {
+        final int deletePosition = position;
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(
+                ViewUploadsActivity.this);
+
+        alert.setTitle("حذف");
+        alert.setMessage("هل تريد حذف الملف ؟*سيتم حذف جميع الملفات!!");
+        alert.setPositiveButton("نعم", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // TOD O Auto-generated method stub
+
+                // main code on after clicking yes
+                uploadList.remove(deletePosition);
+                adapter.notifyDataSetChanged();
+                adapter.notifyDataSetInvalidated();
+
+                mDatabaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                            Upload upload = postSnapshot.getValue(Upload.class);
+                            dataSnapshot.getRef().removeValue();
+                            uploadList.remove(upload);
+                        }
+
+                        uploads = new String[uploadList.size()];
+
+                        for (int i = 0; i < uploads.length; i++) {
+                            uploads[i] = uploadList.get(i).getName();
+                        }
+
+                        //displaying it to list
+                        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, uploads);
+                        listView.setAdapter(adapter);
+                        Intent in=new Intent(context, addCV.class);
+                        context.startActivity(in);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+//                Intent in=new Intent(context, addCV.class);
+//                context.startActivity(in);
+            }
+        });
+        alert.setNegativeButton("لا", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // TODO Auto-generated method stub
+                dialog.dismiss();
+            }
+        });
+
+        alert.show();
+
+    }
+    public void onClick(View view) {
+
+        if(view ==undo){
+            startActivity(new Intent(this, profileCand.class));
+        }
+    }}
 
 
-}
+
+
+
