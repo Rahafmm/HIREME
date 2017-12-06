203f5a144a61b8595380966d2c65cff8d36be21a
HP <rahofa.962hotmail.com>
2017-12-06 09:49:40 +0300

    viewapplication

--- app/src/main/java/com/example/hp/hireme/viewapplication.java
+++ app/src/main/java/com/example/hp/hireme/viewapplication.java
@@ -1,11 +1,13 @@
 package com.example.hp.hireme;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -14,16 +16,20 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class viewapplication extends AppCompatActivity {
-private ListView lisapp;
+ ListView lisapp;
     private FirebaseAuth firebaseAuth;
     String User_id;
     DatabaseReference mDatabaseReference;
     DatabaseReference mDatabaseReference1;
-    List<application> listapp;
+    //List<application> app;
+    ArrayList<application> names;
     String nameOrg;
+    String S;
+    String [] arr;
+    int i=0;
     //Intent intent = getIntent();
    // final String name = intent.getExtras().getString("name", "");
     @Override
@@ -32,6 +38,22 @@ private ListView lisapp;
         setContentView(R.layout.activity_viewapplication);
         lisapp=(ListView)findViewById(R.id.lisapp);
         firebaseAuth = FirebaseAuth.getInstance();
+        arr=new String[100];
+        names=new ArrayList<>();
+        lisapp.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
+                //getting the upload
+                final application upload = names.get(i);
+
+                //Opening the upload file in browser using the upload url
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(upload.getUrl()));
+                startActivity(intent);
+
+
+            }
+        });
         User_id = firebaseAuth.getCurrentUser().getUid();
         mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Org");
 
@@ -43,18 +65,7 @@ private ListView lisapp;
                     if(o.getuid().toUpperCase().equals(User_id.toUpperCase())){
                         nameOrg=o.getname();}
                 }
-                Toast.makeText(viewapplication.this, nameOrg, Toast.LENGTH_LONG).show();
-               /* String[] uploads = new String[listapp.size()];
 
-                for (int i = 0; i < uploads.length; i++) {
-                        uploads[i] = listapp.get(i).getAppname();
-
-
-                }
-
-                //disp laying it to list
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, uploads);
-                lisapp.setAdapter(adapter);*/
             }
 
             @Override
@@ -62,5 +73,38 @@ private ListView lisapp;
 
             }
         });
+        mDatabaseReference1= FirebaseDatabase.getInstance().getReference().child("application");
+        mDatabaseReference1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    application o = postSnapshot.getValue(application.class);
+                    if(o.getNameOrg().toUpperCase().equals(nameOrg.toUpperCase()))
+                        names.add(o);
+                       // Toast.makeText(viewapplication.this, S, Toast.LENGTH_SHORT).show();
+                }
+
+                String[] uploads = new String[names.size()];
+
+                for (int i = 0; i < uploads.length; i++) {
+
+                    uploads[i] = names.get(i).getAppname();
+
+                }
+
+                //disp laying it to list
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, uploads);
+              lisapp.setAdapter(adapter);
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
